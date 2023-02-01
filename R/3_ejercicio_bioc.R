607a8bbd220bbe7034ac214cf98a27f75efa2c99
Aldo Garcia Prado <aldogp@lcg.unam.mx>
2023-02-01 09:20:55 -0600

    Initial commit

---
+++ R/3_ejercicio_bioc.R
@@ -0,0 +1,37 @@
+## García Prado Aldo, Rivera Ramírez Andrés.
+## Statial
+
+## Autores: Farhan Ameen, Sourish Iyengar, Shila Ghazanfar, Ellis Patrick
+
+## Cita: Ameen F, Iyengar S, Ghazanfar S, Patrick E (2022). Statial: A package to identify changes in cell state relative to spatial associations. R package version 1.0.1
+
+## Conjunto de funciones que permite identificar cambios en el estado celular.  Provee cuantificación de la localización de distintios tipos celulares además de cambios
+## en la expresión asociado con niveles variables de localización. Esto permite estudiar como la estrucutura y expresion celulares cambian en relación a los agentes
+## que las rodean.
+
+## Existen estadísticas de descargas correspondientes al año 2022 y 2023. Durante los últimos tres meses del 2022, el paquete fue descargado 126 veces desde 59 IPs
+## distintas, mientras que, durante Enero de 2023 ha sido descargado 51 veces desde 22 IPs distintas.
+
+## Los desarrolladores responden a las preguntas frecuentemente.
+
+## Esta aprobado en todas las plataformas.
+
+## Su rank es casi el último, 2177/2183.
+
+
+##RgnTX
+
+## Autores: Yue Wang, Jia Meng
+
+## Cita: Wang Y, Meng J (2022). RgnTX: Colocalization analysis of transcriptome elements in the presence of isoform heterogeneity and ambiguity. R package version 1.0.0.
+
+## Codigo que funciona para anotar transcriptomas con splicing alternativo complejo.
+## Soporta la anotacion de elementos del transcriptoma con poca claridad acerca de su isoforma.
+
+## Los desarrolladores contestan de forma irregular.
+
+## Rank: 2141/2183
+
+## Aprobado para todas las plataformas.
+
+## Han tenido un total de 206 descargas desde 102 IPs unicops.
