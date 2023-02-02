82a9d5d097ee77a6fbecb6358e82d92cd0951373
Aldo Garcia Prado <aldogp@lcg.unam.mx>
2023-02-02 10:38:25 -0600

    Se agrega notas 6

---
+++ R/06-modelos.R
@@ -0,0 +1,30 @@
+## ?model.matrix
+mat <- with(trees, model.matrix(log(Volume) ~ log(Height) + log(Girth)))
+mat
+
+colnames(mat)
+
+summary(lm(log(Volume) ~ log(Height) + log(Girth), data = trees))
+
+## Datos de ejemplo
+(sampleData <- data.frame(
+  genotype = rep(c("A", "B"), each = 4),
+  treatment = rep(c("ctrl", "trt"), 4)
+))
+
+## Creemos las imágenes usando ExploreModelMatrix
+vd <- ExploreModelMatrix::VisualizeDesign(
+  sampleData = sampleData,
+  designFormula = ~ genotype + treatment,
+  textSizeFitted = 4
+)
+
+## Veamos las imágenes
+cowplot::plot_grid(plotlist = vd$plotlist)
+
+## Usaremos shiny otra vez
+app <- ExploreModelMatrix::ExploreModelMatrix(
+  sampleData = sampleData,
+  designFormula = ~ genotype + treatment
+)
+if (interactive()) shiny::runApp(app)
